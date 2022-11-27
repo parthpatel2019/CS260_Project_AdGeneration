ba6e867985c0e8ba76784b728ee148c628c43471
Parth Patel <parthpatel2019@gmail.com>
2022-11-26 19:14:40 -0800

    Fixed Image Selection Issues
    Used a different API to help with image selection and word association

--- data_collection.py
+++ data_collection.py
@@ -1,4 +1,4 @@
-from bing_image_downloader import downloader
+import simple_image_download.simple_image_download as downloader
 import wikipedia
 import string
 import nltk
@@ -7,13 +7,15 @@ import os
 
 class DataCollection:
     def __init__(self):
-        self.output_directory = './dataset'
+        self.output_directory = './dataset/'
         self.limit = 5
         nltk.download('punkt')
+        self.my_downloader = downloader.Downloader()
+        self.my_downloader.directory = self.output_directory
+        self.my_downloader.extensions = '.jpg'
 
     def download_image(self, input):
-        downloader.download(input, limit=self.limit,  output_dir=self.output_directory,
-                            timeout=5, filter="photo", verbose=False)
+        self.my_downloader.download(input, limit=self.limit)
 
     def remove_existing_directory(self, input):
         dir_path = self.output_directory + '/' + str(input)
@@ -27,26 +29,28 @@ class DataCollection:
         else:
             print("Directory does not exist so continue download...")
 
-    def get_syn(self, input):
+    def get_words(self, input):
         try:
             text = wikipedia.summary(input)
         except:
             print("Error: Cannot find wikipedia summary for this input.")
             print("Try something specific\nExample: Sprite -> Sprite (drink)")
             return None
-        remove_words = list(nltk.corpus.stopwords.words('english')) + list(string.punctuation) + list(["'s"]) + list(['1','2','3','4','5','6','7','8','9','0'])
         word_tokens = nltk.tokenize.word_tokenize(text)
-        filtered_sentence = [w for w in word_tokens if not w.lower() in remove_words]
+        filtered_sentence = [w for w in word_tokens if not w.lower() in list(nltk.corpus.stopwords.words('english'))]
+        filtered_sentence = [w for w in filtered_sentence if not w.lower() in list(string.punctuation)]
+        filtered_sentence = [w for w in filtered_sentence if not "'s" in w.lower()]
+        filtered_sentence = [w for w in filtered_sentence if not any(chr.isdigit() for chr in w.lower())]
+        filtered_sentence = [*set(filtered_sentence)]
         return filtered_sentence
 
     def run(self, input):
-        words_list = self.get_syn(input)
+        words_list = self.get_words(input)
         if(words_list is None):
             return None, None
-        dir_path = self.output_directory + '/' + str(input)
-        image_paths = []
         self.remove_existing_directory(input)
         self.download_image(input)
-        for i in range(self.limit):
-            image_paths.append(dir_path + '/Image_' + str(i))
+        image_paths = []
+        for i in self.my_downloader.cached_urls.keys():
+            image_paths.append(str(self.my_downloader.cached_urls[i][0] + '/' + i))
         return image_paths, words_list
